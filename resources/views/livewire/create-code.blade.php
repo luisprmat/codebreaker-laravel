db6fef1921730d816850928c8622f5d3dabd6ff0
Luis Parrado <luisprmat@gmail.com>
2024-08-03 23:10:34 -0500

    Create my first livewire component

---
+++ resources/views/livewire/create-code.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{ $count }}
+
+    <p>
+        <button wire:click="increment" wire:confirm="¿Está seguro de eso?">Aumentar</button>
+    </p>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
 <x-layout>
     <h1 class="text-red-500 text-xl">Hello world!</h1>
+
+    <livewire:create-code />
 </x-layout>
