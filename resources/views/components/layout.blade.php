06a0ae3a05d3a5610dc7ae035a97719347184682
Luis Parrado <luisprmat@gmail.com>
2024-08-04 01:47:38 -0500

    Create the code legend

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
     @vite(['resources/js/app.js'])
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
-<body class="pt-8 bg-primary">
+<body class="pt-8 bg-primary text-white">
     {{ $slot }}
 </body>
 </html>
